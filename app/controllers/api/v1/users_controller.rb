c91300b6716ad09a7bac58c4e0d5b363b3d0fa5a
Jacob Lam <jlam18@binghamton.edu>
2019-07-19 12:22:16 -0400

    redirect after sign up

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -17,7 +17,9 @@ class Api::V1::UsersController < ApplicationController
     @user.save
 
     if @user.valid?
-      render json: { user: UserSerializer.new(@user) }, status: :created
+
+      token = encode_token({ user_id: @user.id })
+      render json: { user: UserSerializer.new(@user), jwt: token  }, status: :created
     else
       render json: { error: 'failed to create user' }, status: :not_acceptable
     end
